425fd59d6bda47e0526630270f4f6bbaa1290afb
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2010-12-28 12:50:40 +0100

    Working markup tree, parser and HTML generator.

--- kocms/MarkupHtml.hx
+++ kocms/MarkupHtml.hx
@@ -8,64 +8,64 @@ class MarkupHtml {
         return StringTools.replace(StringTools.replace(StringTools.htmlEscape(string), "\"", "&#34;"), "'", "&#39;");
     }
 
-    public static function toHtml(markup: Array<Markup>): String {
+    public static function toHtml(markup: Array<Markup>, ?command: String -> String -> String): String {
         var builder = new StringBuf();
         var output = function(string) { builder.add(string); };
-        writeHtml(markup, output);
+        writeHtml(markup, output, command);
         return builder.toString();
     }
 
-    public static function writeHtml(markup: Array<Markup>, output: String -> Void): Void {
+    public static function writeHtml(markup: Array<Markup>, output: String -> Void, ?command: String -> String -> String): Void {
         for(element in markup) {
-            writeMarkup(element, output);
+            writeMarkup(element, output, command);
         }
     }
 
-    private static function writeMarkup(markup: Markup, output: String -> Void): Void {
+    private static function writeMarkup(markup: Markup, output: String -> Void, ?command: String -> String -> String): Void {
         switch(markup) {
             case Paragraph(inside):
                 output("<p>\n");
-                writeInline(inside, output);
+                writeInline(inside, output, command);
                 output("</p>\n");
             case Heading(inside):
                 output("<h1>\n");
-                writeInline(inside, output);
+                writeInline(inside, output, command);
                 output("</h1>\n");
             case Subheading(inside):
                 output("<h2>\n");
-                writeInline(inside, output);
+                writeInline(inside, output, command);
                 output("</h2>\n");
             case Code(code):
                 output("<pre>\n");
                 output(htmlEscape(code));
                 output("</pre>\n");
             case Listing(listing):
-                writeListing(listing, output);
+                writeListing(listing, output, command);
             case Command(name, arguments):
-                // TODO
+                output(command(name, arguments));
         }
     }
     
-    private static function writeListing(listing: Listing, output: String -> Void): Void {
+    private static function writeListing(listing: Listing, output: String -> Void, ?command: String -> String -> String): Void {
         switch(listing) {
             case Bullets(content, children):
-                writeListingContent(content, output);
+                writeListingContent(content, output, command);
                 if(children.length > 0) {
                     output("<ul>\n");
                     for(child in children) {
                         output("<li>\n");
-                        writeListing(child, output);
+                        writeListing(child, output, command);
                         output("</li>\n");
                     }
                     output("</ul>\n");
                 }
             case Numbers(content, children):
-                writeListingContent(content, output);
+                writeListingContent(content, output, command);
                 if(children.length > 0) {
                     output("<ol>\n");
                     for(child in children) {
                         output("<li>\n");
-                        writeListing(child, output);
+                        writeListing(child, output, command);
                         output("</li>\n");
                     }
                     output("</ol>\n");
@@ -73,19 +73,19 @@ class MarkupHtml {
         }
     }    
 
-    private static function writeListingContent(content: Inline, output: String -> Void): Void {
-        writeInline(content, output);
+    private static function writeListingContent(content: Inline, output: String -> Void, ?command: String -> String -> String): Void {
+        writeInline(content, output, command);
     }
     
-    private static function writeInline(content: Inline, output: String -> Void): Void {
+    private static function writeInline(content: Inline, output: String -> Void, ?command: String -> String -> String): Void {
         switch(content) {
             case Bold(inside):
                 output("<strong>");
-                writeInline(inside, output);
+                writeInline(inside, output, command);
                 output("</strong>");
             case Italic(inside):
                 output("<em>");
-                writeInline(inside, output);
+                writeInline(inside, output, command);
                 output("</em>");
             case Verbatim(verbatim):
                 output("<code>");
@@ -93,13 +93,13 @@ class MarkupHtml {
                 output("</code>");
             case Link(inside, url):
                 output("<a href=\"" + htmlEscape(url) + "\">");
-                writeInline(inside, output);
+                writeInline(inside, output, command);
                 output("</a>");
             case Text(text):
                 output(htmlEscape(text));
             case Sequence(left, right):
-                writeInline(left, output);
-                writeInline(right, output);
+                writeInline(left, output, command);
+                writeInline(right, output, command);
         }
     }
 }
